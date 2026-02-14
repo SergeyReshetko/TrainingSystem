e40985a4bffe6551b2914161e79fbb35ab6bae1a
ReshetkoSergey <dev.rsergey@gmail.com>
2026-02-14 11:47:23 +0300

    Create repository The training system

---
+++ src/main/java/com/trainingsystem/TrainingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.trainingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainingsystem/TrainingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
